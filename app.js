3bf648e0c780db8206e76c877650796522411580
IvoTod <ivo.tod.todorov@gmail.com>
2018-01-29 23:46:59 +0200

    Add session support

--- app.js
+++ app.js
@@ -2,17 +2,36 @@ const express = require('express');
 const path = require('path');
 const favicon = require('serve-favicon');
 const logger = require('morgan');
-const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const session = require('express-session');
+const MySQLStore = require('express-mysql-session')(session);
 
 const index = require('./routes/index');
 const users = require('./routes/users');
+
+let options = {
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'root',
+    database: 'harmonySessions'
+};
+
+let sessionStore = new MySQLStore(options);
 
 const app = express();
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+
+app.use(session({
+    key: 'test',
+    secret: '8nvtCV4Krg',
+    resave: false,
+    store: sessionStore,
+    saveUninitialized: false
+}));
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,8 @@ const connection = require('../db/connection.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.status(200).send();
+    req.session.visitCount = req.session.visitCount ? req.session.visitCount + 1 : 1;
+    res.send("You have visited this page " + req.session.visitCount + " times");
 });
 
 module.exports = router;
